fixed conversion from alpha to numeric arry - need to fix actual distance calculation

src/pyloric/distances.py
@@ -4,17 +4,11 @@ import csv,pandas as pd, numpy as np
 class Distances:
       
     def __init__(self):
-        self.dir="src/pyloric/data"
-        self.fname="combined_spike_patterns.csv" #"temp_patterns.csv" 
+        self.dir ="src/pyloric/data"
+        self.fname = "temp_patterns2.csv" #"combined_spike_patterns.csv" #
     
     def read_file(self):
         full_name=self.dir+'/'+self.fname
-        #input = np.loadtxt(full_name, dtype='i', delimiter=',')
-        #with open(full_name) as csvfile:
-         #   reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-         #   for row in reader:
-         #       print(row[0])
-               # print(', '.join(row[0]))
         df=pd.read_csv(full_name,dtype={'spike_patterns': 'string'},skiprows=0)
         df = df.reset_index()
 
@@ -32,35 +26,35 @@ class Distances:
                 df.at[i,'spike_patterns'] = 'x' 
  
         return df.iloc[:,3].values, max_length
+    
+    def handle_pattern(self):
+        
+        full_name=self.dir+'/'+self.fname
+        for i, df in enumerate(pd.read_csv(full_name, chunksize=1000)):
+            alpha=df['α_fast']
+            beta=df['β_fast']
+            #pattern=df['spike_patterns']
 
-     #def calculate_pairwise(self,v):
-     #   n=len(v)
-     #   print(n*n)
-     #   results=np.zeros((n,n))
-       # print(n)
-     #   for i in range(0,n):
-     #       for j in range(0,i):
-     #           if (i*j)%1000==0:
-     #               print(i,",",j,i*j)
+            max_length=df.spike_patterns.dropna().map(len).max() #if not df.empty() else 0
+            
+            df['converted_spike_pattern']=df['spike_patterns'].str.replace('a','1').str.replace('l','2').str.replace('p','3').str.replace('t','4')
+            df['converted_spike_pattern']=df['converted_spike_pattern'].str.pad(max_length,side='right',fillchar='0')
+            #df['converted']=self.convert_to_num_array(pattern,max_length)
+            df.to_csv(self.dir+'/'+'converted_temp.csv', columns=['α_fast','β_fast','spike_patterns','converted_spike_pattern'], index=False, mode='w', header=True)
 
-     #           results[i,j]=damerau_levenshtein_distance(v[i],v[j])
+ 
 
-     #   print("done")
-     #   return results
-        """
-        function calculate_pairwise(vec,l=false)
+    def convert_to_num_array(self,spike_pattern,max_length):
 
-            n=length(vec)
-            results=zeros(Int32,n,n)
-            Threads.@threads for i in 1:n 
+        converted_patterns=list()
+        for i,pattern in spike_pattern.items():
+            num_array=(self.convert_to_symbol(pattern))
+            num_array=np.pad(num_array,(0,max_length-len(num_array)),'constant')
+            converted_patterns.append(num_array)
 
-                @inbounds for j in 1:i 
-                    results[j,i]=DamerauLevenshtein()(vec[i],vec[j])
-                end
-            end
-            Symmetric(results)
-        end
-        """
+        
+        return np.array(converted_patterns)
+
     def get_spikes_as_num_array(self):
         spike_patterns, max_length = self.read_file()
        
@@ -111,8 +105,9 @@ class Distances:
 #d=damerau_levenshtein_distance('smtih', 'smiadfadsfasdfth')  # expected result: 1
 #print(d)
 dist=Distances()
+dist.handle_pattern()
 #print(vec)
-print(dist.get_spikes_as_num_array())
+#print(dist.get_spikes_as_num_array())
 #print(type(dist.get_spikes_as_num_array()[1]))
 #arr=dist.get_spikes_as_num_array()[1]
 #print(np.pad(arr,(0,90-len(arr)),'constant'))

src/pyloric/distances_gpu.py
@@ -2,14 +2,21 @@ from numba import cuda, numba
 import numpy as np,csv,pandas as pd
 from distances import Distances
 
+
+#class GPUDistance:
+	#def __init__(self):
+	#	self.dir ="src/pyloric/data"
+	#	self.fname = "converted_temp.csv"  
+
+
 @cuda.jit
 def get_string_distances(a,c,):
 	row, col = cuda.grid(2)
 	if row < a.shape[0] and col < a.shape[1]:
 		c[row,col] = string_distance(a[row],a[col])
-    	    
+			
 
-@cuda.jit('int64(float64[:],float64[:])',device=True) 
+@cuda.jit('int64(int64[:],int64[:])',device=True) 
 def string_distance(s1,s2):
 	m=len(s1) 
 	n=len(s2) 
@@ -20,39 +27,73 @@ def string_distance(s1,s2):
 	for i in range(0,n):
 		v1[i]=i
 
-    # Initialize the table
-    # Populate the table using dynamic programming
+	# Initialize the table
+	# Populate the table using dynamic programming
 	for i in range(0, m-1):
 		v2[0]=i+1
 		for j in range(0, n-1):
 			deleteCost=v1[j+1] + 1
 			insertCost=v2[j] + 1
-			 
+			
 			if s1[i] == s2[j]:
 				substitutionCost=v1[j]
 			else:
 				substitutionCost=v1[j]+1
 
 			v2[j+1]=min(deleteCost,insertCost,substitutionCost)
-        
+		
 	v1=v2
 	return v1[n-1]
+
+def convert_to_num_array(spike_pattern):
+	converted_patterns=list()
+	for i,pattern in spike_pattern.items():
+		#num_array=(self.convert_to_symbol(pattern))
+		converted_patterns.append(num_array)
+		
+	return np.array(converted_patterns)
+
+def read_converted_spikes():
+	full_name = "src/pyloric/data/converted_temp.csv" 
+
+	df=pd.read_csv(full_name,dtype={'converted_spike_pattern': 'string'},skiprows=0)
+	df = df.reset_index()
+
+	lst=list()
+	for i, row in df.iterrows():
+		pattern = df.at[i,'converted_spike_pattern']
+		#print(convert_to_array(pattern))
+		lst.append(convert_to_array(pattern))
+		#length = 0 if pd.isnull(pattern) else len(pattern)
+
+	#return df.iloc[:,4].values #, max_length   
+	return lst	
+
+def convert_to_array(pattern):
+	lst=list()
+	for s in pattern:
+		lst.append(int(s))
+	
+	return np.array(lst)
+
 
 def find_distance():
 	threads_per_block = 256
 	blocks_per_grid = 256#(N + threads_per_block - 1) // threads_per_block
 
 	# Copy data to the device
-	a=[1,2,2,3,6]
-	b=[4,5,7,1,2]
-	c=[4,5,7,3,1]
-	d=[1,3,2,2,5]
- 
+	a=[1,2,2,3,6,6]
+	b=[4,5,7,1,2,6]
+	c=[4,5,7,3,1,7]
+	d=[1,3,2,2,5,7]
+	e=[1,3,2,2,6,7]
+
 	
-	#a1=np.array([a,b,c,d])
+	a1=np.array([a,b,c,d,e])
 	#print("type is: ",type(a1))
-	dist = Distances()
-	a1=dist.get_spikes_as_num_array()
+	#dist = Distances()
+	#a1=dist.get_spikes_as_num_array()
+	#a1=np.array(read_converted_spikes())
 	print("type is: ",type(a1))
 	print("shape: (",a1.shape[0],a1.shape[1],")")
 	#a1=np.ndarray(len([a,b,c,d]), dtype=np.ndarray)
@@ -60,13 +101,15 @@ def find_distance():
 	#a2=cuda.to_device(b)
 	#gpu_results=cuda.to_device(np.ndarray(shape=(a1.shape[0],a1.shape[0])))
 	
-	gpu_results=cuda.to_device(np.ndarray(shape=(len(a1),len(a1[0]))))
+	gpu_results=cuda.to_device(np.ndarray(shape=(len(a1),len(a1))))
 	# Launch the kernel
 	get_string_distances[blocks_per_grid, threads_per_block](a1,gpu_results)
 	res = gpu_results.copy_to_host()
 	print("Got ",len(res), " results")
 	print("shape: ",res.shape)
+	print(res)
  
 
+#find_distance()
 find_distance()
- 
+#print(read_converted_spikes())